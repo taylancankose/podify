af49b57b4c778e914f8773345faa05b0f767e4eb
Taylan <94180821+taylancankose@users.noreply.github.com>
2024-05-09 13:57:46 +0300

    Add files via upload

---
+++ PodifyPlayer/babel.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+          '@ui': './src/ui',
+          src: './src',
+        },
+      },
+    ],
+  ],
+};
---
+++ PodifyPlayer/src/ui/AppInput.tsx
@@ -0,0 +1,29 @@
+import colors from '@utils/colors';
+import {FC} from 'react';
+import React = require('react');
+import {TextInputProps, StyleSheet, TextInput} from 'react-native';
+
+interface Props extends TextInputProps {}
+
+const AppInput: FC<Props> = props => {
+  return (
+    <TextInput
+      style={styles.input}
+      {...props}
+      placeholderTextColor={colors.INACTIVE_CONTRAST}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    borderWidth: 2,
+    borderColor: colors.SECONDARY,
+    height: 45,
+    borderRadius: 25,
+    color: colors.CONTRAST,
+    padding: 10,
+  },
+});
+
+export default AppInput;
