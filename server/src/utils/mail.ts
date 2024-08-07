c66da64b4db2a380fca8b7facac1b26d388948e0
Taylan <taylancankose@gmail.com>
2024-08-07 19:23:22 +0300

    mailtrap template changes

--- server/src/utils/mail.ts
+++ server/src/utils/mail.ts
@@ -1,15 +1,17 @@
 import { generateTemplate } from "#/mail/template";
 import {
   LOGIN_URL,
+  MAILTRAP_ENDPOINT,
   MAILTRAP_HOST,
   MAILTRAP_PASSWORD,
-  MAILTRAP_PORT,
+  MAILTRAP_TOKEN,
   MAILTRAP_USER,
   VERIFICATION_EMAIL,
 } from "./variables";
 import nodemailer from "nodemailer";
 import path from "path";
 const { MailtrapClient } = require("mailtrap");
+import fs from "fs";
 
 export const generateMailTransporter = () => {
   const transport = nodemailer.createTransport({
@@ -31,29 +33,65 @@ interface Profile {
 }
 
 export const sendVerificationMail = async (token: string, profile: Profile) => {
-  const transport = generateMailTransporter();
-  const { name, email, userId } = profile;
+  const { name, email } = profile;
 
-  transport.sendMail({
-    to: email,
-    from: VERIFICATION_EMAIL,
-    subject: "You need to verify your account",
-    html: generateTemplate({
-      title: `Hello ${name}`,
-      message:
-        "Welcome to the Podify! There are so much thing that we do for verified users. Use the given OTP to verify your email",
-      logo: "cid:logo",
-      link: "#",
-      btnTitle: "Verify the Account",
-      code: token,
-    }),
-    attachments: [
-      {
-        filename: "logo.jpg",
-        path: path.join(__dirname, "../mail/images/logo.jpg"),
-        cid: "logo", // content id
-      },
-    ],
+  const welcomeMsg =
+    "Welcome to the Podify! There are so much thing that we do for verified users. Use the given OTP to verify your email";
+
+  const client = new MailtrapClient({
+    endpoint: MAILTRAP_ENDPOINT,
+    token: MAILTRAP_TOKEN,
+  });
+
+  const sender = {
+    email: VERIFICATION_EMAIL,
+    name: "You need to verify your Podify account",
+  };
+  const recipients = [
+    {
+      email: email,
+    },
+  ];
+
+  const logoImg = fs.readFileSync(
+    path.join(__dirname, "../mail/images/logo.jpg")
+  );
+
+  // client
+  //   .send({
+  //     from: sender,
+  //     to: recipients,
+  //     subject: "Verifiction email",
+  //     html: generateTemplate({
+  //       title: `Hello ${name}`,
+  //       message:
+  //         "Welcome to the Podify! There are so much thing that we do for verified users. Use the given OTP to verify your email",
+  //       logo: "cid:logo",
+  //       link: "#",
+  //       btnTitle: "Verify the Account",
+  //       code: token,
+  //     }),
+  //     category: "Verifiction Mail",
+  //     attachments: [
+  //       {
+  //         filename: "logo.jpg",
+  //         content_id: "logo",
+  //         disposition: "inline",
+  //         content: logoImg,
+  //         type: "image/jpg",
+  //       },
+  //     ],
+  //   })
+  //   .then(console.log, console.error);
+
+  client.send({
+    from: sender,
+    to: recipients,
+    template_uuid: "ce1e6712-a9b1-4a3d-bc8c-64e41b739746",
+    template_variables: {
+      user_name: name,
+      token: token,
+    },
   });
 };
 
@@ -66,79 +104,100 @@ interface Options {
 }
 
 export const sendForgetPasswordLink = async (options: Options) => {
-  const transport = generateMailTransporter();
+  // const transport = generateMailTransporter();
   const { email, link } = options;
 
-  transport.sendMail({
-    to: email,
-    from: VERIFICATION_EMAIL,
-    subject: "Reset Password Link",
-    html: generateTemplate({
-      title: `Hello `,
-      message:
-        "We just received a request that you forgot your password. You can use the link below and create new password",
-      logo: "cid:logo",
-      link,
-      btnTitle: "Reset Password",
-    }),
-    attachments: [
-      {
-        filename: "logo.jpg",
-        path: path.join(__dirname, "../mail/images/logo.jpg"),
-        cid: "logo", // content id
-      },
-    ],
+  const client = new MailtrapClient({
+    endpoint: MAILTRAP_ENDPOINT,
+    token: MAILTRAP_TOKEN,
   });
+
+  const sender = {
+    email: VERIFICATION_EMAIL,
+    name: "Password Reset",
+  };
+  const recipients = [
+    {
+      email: email,
+    },
+  ];
+
+  client.send({
+    from: sender,
+    to: recipients,
+    template_uuid: "59004a70-67d7-444c-9e43-0f0993f4dd81",
+    template_variables: {
+      user_email: "email",
+      pass_reset_link: link,
+    },
+  });
+
+  // transport.sendMail({
+  //   to: email,
+  //   from: VERIFICATION_EMAIL,
+  //   subject: "Reset Password Link",
+  //   html: generateTemplate({
+  //     title: `Hello `,
+  //     message:
+  //       "We just received a request that you forgot your password. You can use the link below and create new password",
+  //     logo: "cid:logo",
+  //     link,
+  //     btnTitle: "Reset Password",
+  //   }),
+  //   attachments: [
+  //     {
+  //       filename: "logo.jpg",
+  //       path: path.join(__dirname, "../mail/images/logo.jpg"),
+  //       cid: "logo", // content id
+  //     },
+  //   ],
+  // });
 };
+
 export const sendPasswordResetSuccessEmail = async (
   name: string,
   email: string
 ) => {
-  const transport = generateMailTransporter();
+  // const transport = generateMailTransporter();
 
-  transport.sendMail({
-    to: email,
-    from: VERIFICATION_EMAIL,
-    subject: "Password Reset Success",
-    html: generateTemplate({
-      title: `Hello ${name}`,
-      message:
-        "We just updated your password. You can now loging with your new password",
-      logo: "cid:logo",
-      link: LOGIN_URL,
-      btnTitle: "Reset Password",
-    }),
-    attachments: [
-      {
-        filename: "logo.jpg",
-        path: path.join(__dirname, "../mail/images/logo.jpg"),
-        cid: "logo", // content id
-      },
-    ],
+  const client = new MailtrapClient({
+    endpoint: MAILTRAP_ENDPOINT,
+    token: MAILTRAP_TOKEN,
   });
-};
 
-const TOKEN = "d644f75fd63a1cbc0424e1768dec0972";
-const ENDPOINT = "https://send.api.mailtrap.io/";
+  const sender = {
+    email: VERIFICATION_EMAIL,
+    name: "Password Reset Success",
+  };
 
-const client = new MailtrapClient({ endpoint: ENDPOINT, token: TOKEN });
-
-const sender = {
-  email: "mailtrap@podify.store",
-  name: "Mailtrap Test",
-};
-const recipients = [
-  {
-    email: "projectsofteco@gmail.com",
-  },
-];
-
-client
-  .send({
+  client.send({
     from: sender,
-    to: recipients,
-    subject: "You are awesome!",
-    text: "Congrats for sending test email with Mailtrap!",
-    category: "Integration Test",
-  })
-  .then(console.log, console.error);
+    to: [{ email }],
+    template_uuid: "5a5c80f6-04f6-4c07-af0f-0ffa05c2f923",
+    template_variables: {
+      user_name: name,
+      user_email: email,
+    },
+  });
+
+  // transport.sendMail({
+  //   to: email,
+  //   from: VERIFICATION_EMAIL,
+  //   subject: "Password Reset Success",
+  //   html: generateTemplate({
+  //     title: `Hello ${name}`,
+  //     message:
+  //       "We just updated your password. You can now loging with your new password",
+  //     logo: "cid:logo",
+  //     link: LOGIN_URL,
+  //     btnTitle: "Reset Password",
+  //   }),
+  //   attachments: [
+  //     {
+  //       filename: "logo.jpg",
+  //       path: path.join(__dirname, "../mail/images/logo.jpg"),
+  //       cid: "logo", // content id
+  //     },
+  //   ],
+  // });
+};
