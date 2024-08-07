de2d69a82cc4035cada69c0867e755f8b6149ccf
Taylan <taylancankose@gmail.com>
2024-08-07 16:37:42 +0300

    mailtrap mail setup

--- server/src/utils/mail.ts
+++ server/src/utils/mail.ts
@@ -9,6 +9,7 @@ import {
 } from "./variables";
 import nodemailer from "nodemailer";
 import path from "path";
+const { MailtrapClient } = require("mailtrap");
 
 export const generateMailTransporter = () => {
   const transport = nodemailer.createTransport({
@@ -116,3 +117,28 @@ export const sendPasswordResetSuccessEmail = async (
     ],
   });
 };
+
+const TOKEN = "d644f75fd63a1cbc0424e1768dec0972";
+const ENDPOINT = "https://send.api.mailtrap.io/";
+
+const client = new MailtrapClient({ endpoint: ENDPOINT, token: TOKEN });
+
+const sender = {
+  email: "mailtrap@podify.store",
+  name: "Mailtrap Test",
+};
+const recipients = [
+  {
+    email: "projectsofteco@gmail.com",
+  },
+];
+
+client
+  .send({
+    from: sender,
+    to: recipients,
+    subject: "You are awesome!",
+    text: "Congrats for sending test email with Mailtrap!",
+    category: "Integration Test",
+  })
+  .then(console.log, console.error);
